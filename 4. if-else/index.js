e780fb5899d2af4ee94436da61b43cee5e942beb
Umid <wa019960596@gmail.com>
2023-08-23 12:10:32 +0500

    4. if-else homework

---
+++ 4. if-else/index.js
@@ -0,0 +1,16 @@
+// if ("0") {
+//     alert('hello')
+// }
+
+// hech nima chiqmaydi
+
+// let a = 2
+// let b = 3
+
+// let result = (a + b < 4) ? 'Below' : 'over';
+
+
+// let message = (login == 'employee') ? 'hello' :
+// (login == 'director') ? 'greetings' :
+// (login == '') ? 'no login' :
+// '';
